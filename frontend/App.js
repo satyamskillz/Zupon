932ecbe7cce2dec02fffc23220a217c2829c5198
Rohit Kumar <rk212300@outlook.com>
2022-07-30 10:47:03 +0530

    landing-page started

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import LandingPage from './src/containers/landing_page/index'
 function App(props) {
     return (
         <>
+            <LandingPage />
         </>
     );
 }
