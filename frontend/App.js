bbacf6da0c62fc56eccdd9cb717695a37b2670ab
Satyam sharma <satyamskillz@gmail.com>
2022-08-12 23:34:57 +0530

    Added Navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import HomeScreen from './src/screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 
 function App(props) {
-
-    return (
-        <HomeScreen />
-    );
+  // Check if the user is signed in or not
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={HomeScreen} />
+        <Stack.Screen name="Dashboard" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
